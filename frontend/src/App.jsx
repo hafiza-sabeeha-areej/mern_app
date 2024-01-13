df514d1f15d3dae2f2036b1932e4c2eb38f14820
AmmQamar <qamarsheikhlhr@gmail.com>
2024-01-14 01:05:06 +0500

    CSS
    template CSS setting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./assets/css/all.min.css";
+
+import "./assets/bootstrap/css/bootstrap.min.css";
+
+import "./assets/css/magnific-popup.css";
+import "./assets/css/animate.css";
+import "./assets/css/meanmenu.min.css";
+import "./assets/css/responsive.css";
+
+import ProductItem from "./components/ProductItem";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductItem />
+    </div>
+  );
+}
+
+export default App;
