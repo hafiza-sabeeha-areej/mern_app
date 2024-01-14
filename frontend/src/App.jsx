4410ce27967d8838d56a4ce0fc5d7340c526edb0
AmmQamar <qamarsheikhlhr@gmail.com>
2024-01-14 17:25:54 +0500

    change tamp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./assets/css/all.min.css";
 
-import "./assets/bootstrap/css/bootstrap.min.css";
-
-import "./assets/css/magnific-popup.css";
-import "./assets/css/animate.css";
-import "./assets/css/meanmenu.min.css";
-import "./assets/css/responsive.css";
-
-import ProductItem from "./components/ProductItem";
+import "./vendor/bootstrap/css/bootstrap.min.css";
+import "./fonts/font-awesome-4.7.0/css/font-awesome.min.css";
+import "./fonts/iconic/css/material-design-iconic-font.min.css";
+import "./fonts/linearicons-v1.0.0/icon-font.min.css";
+import "./vendor/animate/animate.css";
+import "./vendor/css-hamburgers/hamburgers.min.css";
+import "./vendor/animsition/css/animsition.min.css";
+import "./vendor/select2/select2.min.css";
+import "./vendor/daterangepicker/daterangepicker.css";
+import "./vendor/slick/slick.css";
+import "./vendor/MagnificPopup/magnific-popup.css";
+import "./vendor/perfect-scrollbar/perfect-scrollbar.css";
+import "./css/util.css";
+import "./css/main.css";
 
 function App() {
-  return (
-    <div className="App">
-      <ProductItem />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
